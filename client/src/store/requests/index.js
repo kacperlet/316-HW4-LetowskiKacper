d0592f5a7c8cdb85e2a59110e98fd136bc776f4b
Kacper Letowski <kacperlet27@gmail.com>
2025-10-24 00:58:34 -0400

    replaced axios with fetch for store

--- client/src/store/requests/index.js
+++ client/src/store/requests/index.js
@@ -10,11 +10,37 @@
     @author McKilla Gorilla
 */
 
-import axios from 'axios'
-axios.defaults.withCredentials = true;
-const api = axios.create({
-    baseURL: 'http://localhost:4000/store',
-})
+const baseURL =  'http://localhost:4000/store';
+
+async function request(method, url, data)
+{
+    let payload = {
+        method: method,
+        mode: 'cors',  
+        cache: 'no-cache',  
+        credentials: 'include',  
+        headers: {  
+            'Content-Type': 'application/json'  
+        },  
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer', 
+    }
+    if (data)
+        payload['body'] = JSON.stringify(data);
+
+    const response = await fetch(baseURL + url, payload);
+
+    // create an object similar in structure to an axios response
+    const responseObject = {
+        status: response.status,
+    }
+    if (response.headers.get("content-length") != 0)
+    {
+        responseObject["data"] = await response.json();
+    }
+
+    return responseObject;
+}
 
 // THESE ARE ALL THE REQUESTS WE`LL BE MAKING, ALL REQUESTS HAVE A
 // REQUEST METHOD (like get) AND PATH (like /top5list). SOME ALSO
@@ -23,18 +49,18 @@ const api = axios.create({
 // WE NEED TO PUT THINGS INTO THE DATABASE OR IF WE HAVE SOME
 // CUSTOM FILTERS FOR QUERIES
 export const createPlaylist = (newListName, newSongs, userEmail) => {
-    return api.post(`/playlist/`, {
+    return request('POST', `/playlist/`, {
         // SPECIFY THE PAYLOAD
         name: newListName,
         songs: newSongs,
         ownerEmail: userEmail
     })
 }
-export const deletePlaylistById = (id) => api.delete(`/playlist/${id}`)
-export const getPlaylistById = (id) => api.get(`/playlist/${id}`)
-export const getPlaylistPairs = () => api.get(`/playlistpairs/`)
+export const deletePlaylistById = (id) => request('DELETE', `/playlist/${id}`)
+export const getPlaylistById = (id) => request('GET', `/playlist/${id}`)
+export const getPlaylistPairs = () => request('GET', `/playlistpairs/`)
 export const updatePlaylistById = (id, playlist) => {
-    return api.put(`/playlist/${id}`, {
+    return request('PUT', `/playlist/${id}`, {
         // SPECIFY THE PAYLOAD
         playlist : playlist
     })
